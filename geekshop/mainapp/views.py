fded67557a67882bd400b8d05a5948ded2802d11
alim <mamutov.creation@yandex.ru>
2022-11-22 15:01:42 +0300

    add static

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.tml')
+    return render(request, 'index.html')
 
 
 def products(request):
-    return render(request, 'products.tml')
+    return render(request, 'products.html')
