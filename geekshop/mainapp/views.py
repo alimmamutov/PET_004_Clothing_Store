b853052cf94399955926cf8015aaaa8f50832d67
alim <mamutov.creation@yandex.ru>
2022-11-22 14:39:04 +0300

    add templates index and products add controllers

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.tml')
+
+
+def products(request):
+    return render(request, 'products.tml')
