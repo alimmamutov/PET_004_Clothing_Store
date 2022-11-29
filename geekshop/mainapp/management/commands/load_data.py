4052048b0993619450ca01dc557aa5d8b42ac045
alim <mamutov.creation@yandex.ru>
2022-11-30 01:05:36 +0300

    lesson3

---
+++ geekshop/mainapp/management/commands/load_data.py
@@ -0,0 +1,42 @@
+from django.core.management.base import BaseCommand, CommandError
+from mainapp.models import Product, ProductCategory
+from django.contrib.auth.models import User
+from os import path
+import json
+
+
+def load_from_json(file_name):
+    parent_dir = path.dirname(path.abspath(__file__))
+    with open(path.join(parent_dir, file_name), 'r', encoding='utf-8') as fh:  # открываем файл на чтение
+        data = json.load(fh)  # загружаем из файла данные в словарь data
+        return data
+
+
+class Command(BaseCommand):
+    help = 'Displays current time'
+
+    def handle(self, *args, **kwargs):
+        # Creating superuser
+        if User.objects.filter(is_superuser=True).count() == 0:
+            try:
+                User.objects.create_superuser(username='admin', email='', password='1',)
+                self.stdout.write("Superuser was created successfully")
+            except Exception:
+                raise CommandError('Error - Creating superuser')
+        else:
+            self.stdout.write("Superuser wasn't created because it already exists")
+
+        # filling DB
+        Product.objects.all().delete()
+        ProductCategory.objects.all().delete()
+        categories_dict = load_from_json('../../fixtures/categories.json')
+        for category in categories_dict:
+            fields = category['fields']
+            ProductCategory.objects.create(id=category['pk'], **fields)
+        self.stdout.write("Table category was filled successfully")
+        products_dict = load_from_json('../../fixtures/products.json')
+        for product in products_dict:
+            fields = product['fields']
+            category = ProductCategory.objects.get(id=fields.pop('category'))
+            Product.objects.create(id=product['pk'], category=category, **fields)
+        self.stdout.write("Table products was filled successfully")
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -2,10 +2,58 @@ from django.shortcuts import render
 
 
 # Create your views here.
+import json
+from os import path
+
+
+def load_from_json(file_name):
+    parent_dir = path.dirname(path.abspath(__file__))
+    with open(path.join(parent_dir, file_name), 'r', encoding='utf-8') as fh:  # открываем файл на чтение
+        data = json.load(fh)  # загружаем из файла данные в словарь data
+        return data
+
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'title': 'GeekShop 111',
+        'header': 'Welcome to my shop',
+        'user': 'Alim',
+        'products': [
+            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 200},
+            {'name': 'Синяя куртка The North Face', 'price': 300},
+            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 500},
+            {'name': 'Черный рюкзак Nike Heritage', 'price': 122},
+            {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex', 'price': 250},
+            {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN', 'price': 380},
+        ],
+
+    }
+    return render(request, 'mainapp/index.html', context)
 
 
 def products(request):
-    return render(request, 'products.html')
+    context = {
+        'title': 'Mamutov Alim\'s Shop' ,
+        'header': 'Welcome to my shop',
+        'user': 'Alim',
+        'products': load_from_json('fixtures/goods.json'),
+    }
+    return render(request, 'mainapp/products.html', context)
+
+
+def test(request):
+    context = {
+        'title': 'GeekShop',
+        'header': 'Welcome to my shop',
+        'user': 'Alim',
+        'products': [
+            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 200},
+            {'name': 'Синяя куртка The North Face', 'price': 300},
+            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 500},
+            {'name': 'Черный рюкзак Nike Heritage', 'price': 122},
+            {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex', 'price': 250},
+            {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN', 'price': 380},
+        ],
+
+    }
+    return render(request, 'mainapp/test_content.html', context)
