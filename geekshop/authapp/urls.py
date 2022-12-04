8552426db8ae5cc2c9e53fc5b8bb44b383182c20
alim <mamutov.creation@yandex.ru>
2022-12-04 18:20:15 +0300

    - authapp created - user model created - settings default user model is from authapp

---
+++ geekshop/authapp/urls.py
@@ -0,0 +1,12 @@
+
+
+from django.urls import path
+
+from django.conf.urls.static import static
+
+# from mainapp.views import products, test
+app_name = 'authapp'
+urlpatterns = [
+    # path('', products, name='products'),
+]
+
