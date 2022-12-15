4cd0e9317683094438b1a9f03cf5babc097b4883
alim <mamutov.creation@yandex.ru>
2022-12-15 21:35:34 +0300

    logout

--- geekshop/authapp/urls.py
+++ geekshop/authapp/urls.py
@@ -4,12 +4,13 @@ from django.urls import path
 
 from django.conf.urls.static import static
 
-from authapp.views import register, login
+from authapp.views import register, login, logout
 
 # from mainapp.views import products, test
 app_name = 'authapp'
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
 ]
 
